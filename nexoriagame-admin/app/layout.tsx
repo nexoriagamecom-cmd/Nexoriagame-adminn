ef8f5196edcf633bdac424951d2be065908813d0
nexoriagamecom-cmd <nexoriagamecom@gmail.com>
2026-05-30 10:54:02 +0100

    Delete nexoriagame-admin directory

--- nexoriagame-admin/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body style={{ margin: 0, background: '#0a0a0a', color: '#fff' }}>
-        {children}
-      </body>
-    </html>
-  )
-}
